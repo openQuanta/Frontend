12528a1df272765894bdcacf4de25c0f7eeeb998
Divine Orji <dpkreativ@gmail.com>
2025-09-12 18:15:52 +0100

    hotfixes: header content updated, and hero section updates. looking good

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,63 +7,67 @@ export default function Home() {
   return (
     <main className="font-sans flex flex-col gap-20">
       {/* Hero */}
-      <section
-        style={{ backgroundImage: `url(${heroArt.src})` }}
-        className="w-full max-w-[1200px] mx-auto bg-contain bg-center md:bg-position-[90%] bg-no-repeat"
-      >
-        <div className="px-5 pt-40 pb-20 w-full max-w-[600px] flex flex-col gap-10 items-start bg-black/30 backdrop-blur-sm">
-          <div className="flex items-center gap-2 p-2 px-4 border border-white/30 rounded-full">
-            <div className="w-3 h-3 rounded-full bg-violet-600"></div>
-            <h4>On Chain & Transparent</h4>
+      <section className="w-full max-w-[1200px] mx-auto">
+        {/* top section */}
+        <section
+          style={{ backgroundImage: `url(${heroArt.src})` }}
+          className="w-full bg-contain bg-center md:bg-position-[90%] bg-no-repeat"
+        >
+          <div className="text-center md:text-left px-5 py-40 w-full max-w-[600px] flex flex-col items-center gap-10 md:items-start bg-black/30 backdrop-blur-sm">
+            <div className="flex items-center gap-2 p-2 px-4 border border-white/30 rounded-full">
+              <div className="w-3 h-3 rounded-full bg-violet-600"></div>
+              <h4>On Chain & Transparent</h4>
+            </div>
+
+            <div className="flex flex-col gap-10">
+              <h1 className="text-5xl">
+                Advancing human progress with publishing
+              </h1>
+              <p>
+                A decentralized platform where researchers can publish openly,
+                mint authorship NFTs, receive bids from readers and
+                institutions, and get reviewed in public by reputation-weighted
+                peers.
+              </p>
+            </div>
+
+            <div className="grid md:flex items-center gap-5">
+              <Button variant="outline">Get Started</Button>
+              <Button variant="link">
+                Become a founding contributor <ChevronRight />
+              </Button>
+            </div>
+          </div>
+        </section>
+
+        {/* bottom section with illustration */}
+        <section className="-mt-20 px-5">
+          <div className="relative w-full max-w-[1000px] mx-auto">
+            <Image src={heroDash} alt="Hero Dashboard" />
           </div>
 
-          <div className="flex flex-col gap-10">
-            <h1 className="text-5xl">
-              Advancing human progress with publishing
-            </h1>
-            <p>
-              A decentralized platform where researchers can publish openly,
-              mint authorship NFTs, receive bids from readers and institutions,
-              and get reviewed in public by reputation-weighted peers.
+          <div className="text-center grid gap-5 py-10">
+            <p className="text-sm md:text-xl">
+              We're backed by the strength of the Solana ecosystem
             </p>
-          </div>
-
-          <div className="flex items-center gap-5">
-            <Button variant="outline">Get Started</Button>
-            <Button variant="link">
-              Become a founding contributor <ChevronRight />
-            </Button>
-          </div>
-        </div>
-      </section>
-
-      <section className="-mt-20">
-        <div className="relative w-full max-w-[1000px] mx-auto px-5">
-          <Image src={heroDash} alt="Hero Dashboard" />
-        </div>
-
-        <div className="w-max mx-auto text-center grid gap-5">
-          <p className="text-xl">We're backed by the strength of the Solana</p>
-          <div className="flex gap-5 px-10">
-            <div className="relative w-full h-4">
-              <Image
-                src={solana}
-                alt="Solana logo"
-                fill
-                className="object-contain"
-              />
-            </div>
-            <div className="relative w-full h-4">
-              <Image
-                src={superteam}
-                alt="Superteam logo"
-                fill
-                className="object-contain h-full"
-              />
+            <div className="flex justify-center items-center gap-5 px-10">
+              <div className="relative">
+                <Image src={solana} alt="Solana logo" className="h-4 w-max" />
+              </div>
+              <div className="relative">
+                <Image
+                  src={superteam}
+                  alt="Superteam logo"
+                  className="h-4 w-max"
+                />
+              </div>
             </div>
           </div>
-        </div>
+        </section>
       </section>
+
+      {/* Smart Flow */}
+      <section></section>
     </main>
   );
 }
