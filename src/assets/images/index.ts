8385dd113db41b7eb7ec4dde38b921a4af2e7780
Benny <mbkc890@gmail.com>
2025-09-15 20:21:29 -0700

    chore: almost done with the contributor page. correctly on the animated background

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -5,6 +5,12 @@ import heroDash from "./hero_dashboard.png";
 import smartFlow from "./smart_flow_demo.png";
 import solanaCode from "./solana_code.png";
 import triangleBlur from "./triangle_blur.png";
+import circleTick from "./circle-tick.svg";
+import YoutubeIcon from "./Link - Youtube@4x (1).png"
+import LinkedinIcon from "./Link - LinkedIn.png"
+import TwitterIcon from "./Link - Whop's Twitter → SVG.png"
+import DiscordIcon from "./Link - Whop's Discord → SVG.png"
+
 
 // partner brands
 import solana from "./solana.png";
@@ -20,4 +26,9 @@ export {
   solana,
   superteam,
   triangleBlur,
+  circleTick,
+  YoutubeIcon,
+  LinkedinIcon,
+  TwitterIcon,
+  DiscordIcon,
 };
